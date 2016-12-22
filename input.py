acdbcb24393e3a3cbd4ec253a0623a450de16098
Chris Gill <chrisraygill@gmail.com>
2016-12-21 22:21:44 -0600

    New project files added

---
+++ input.py
@@ -0,0 +1,1 @@
+#This file will hold all of the functions necessary to read a text file as input and and interpet it
---
+++ main.py
@@ -0,0 +1,1 @@
+#This will be the main part of the program that actually builds the schedule
