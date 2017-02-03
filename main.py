86fdd72308e264430fa0dd2f9773b67e13062a84
Chris Gill <chrisraygill@gmail.com>
2017-02-03 13:06:49 -0600

    fixed indentation

--- main.py
+++ main.py
@@ -2,38 +2,39 @@
 #that actually builds the schedule
 class Course(object):
 
-        #Initialize object
-	def __init__(self, name, hours,
+    #Initialize object
+    def __init__(self, name, hours,
                  diff_rate, time_consump):
-	    self.name = name
-	    self.hours = hours
-	    self.diff_rate = diff_rate
-	    self.time_consump = time_consump
+	self.name = name
+	self.hours = hours
+	self.diff_rate = diff_rate
+	self.time_consump = time_consump
 
-	#Returns string of info for each class so we can print them
-	def __repr__(self):
-	    return "%s, %s, %s, %s" %(self.name, self.hours,
+    #Returns string of info for each class so we can print them
+    def __repr__(self):
+	return "%s, %s, %s, %s" %(self.name, self.hours,
                                   self.diff_rate,
                                   self.time_consump)
 
 def main():
-	#Open file
-	in_file = open("./test.txt" , "r")
+    #Open file
+    in_file = open("./test.txt" , "r")
 
-	#Get rid of headers
-	for i in range(4):
-	    in_file.readline()
+    #Get rid of headers
+    for i in range(4):
+	in_file.readline()
 
-	#Parses through file and creates instances of Course class
-	for line in in_file:
-	    line = line.strip()
-	    line = line.split(", ")
-	    name = Course(line[0], int(line[1]),
+    #Parses through file and creates instances of Course class
+    for line in in_file:
+	line = line.strip()
+	line = line.split(", ")
+	name = Course(line[0], int(line[1]),
                       int(line[2]), int(line[3]))
-	    print(name)
+	print(name)
 
         #Close file
-        in_file.close()
+    in_file.close()
+
 main()
 
 #test
