4de9a20e128130c8d5149d9bd806803710bf65c2
Isabel Cachola <cachola.isabel@gmail.com>
2017-02-03 17:27:46 -0600

    Sorted courses by rating

--- main.py
+++ main.py
@@ -3,15 +3,17 @@
 class Course(object):
 
     #Initialize object
-    def __init__(self, name, hours, diff_rate, time_consump):
+    def __init__(self, name, hours, diff_rate, time_consump, rating):
         self.name = name
         self.hours = hours
         self.diff_rate = diff_rate
         self.time_consump = time_consump
+        self.rating = rating
 
     #Returns string of info for each class so we can print them
-    def __repr__(self):
-        return "%s, %s, %s, %s" %(self.name, self.hours, self.diff_rate, self.time_consump)
+    def __str__(self):
+        #return "%s, %s, %s, %s" %(self.name, self.hours, self.diff_rate, self.time_consump)
+        return str(self.name)
 
 def main():
     #Open file
@@ -21,17 +23,27 @@ def main():
     for i in range(4):
     	in_file.readline()
 
+    lst = [] # List of courses
+    plan = {} # Dictionary of semester:course to take
+
     #Parses through file and creates instances of Course class
     for line in in_file:
         line = line.strip()
         line = line.split(", ")
-        name = Course(line[0], int(line[1]), int(line[2]), int(line[3]))
-        print(name)
+        name = Course(line[0], int(line[1]), int(line[2]), int(line[3]), (int(line[2]) + int(line[3])) / 2)
+        lst.append(name)
 
     #Close file
     in_file.close()
 
-main()
+    # Sorts by rating from largest to smallest
+    for i in range(len(lst)):
+        for j in range(i):
+            if lst[i].rating > lst[j].rating:
+                lst[i], lst[j] = lst[j], lst[i]
 
-#test
-#test2
+    # Prints course and its rating
+    for item in lst:
+        print(item, item.rating)
+
+main()
