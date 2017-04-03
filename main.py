22aaa6fc207ce810e80e2e34c313954bc397f54c
Christopher R. Gill <chrisraygill@utexas.edu>
2017-04-03 15:15:11 -0500

    switched to greedy algorithm for class placement, optimal rating distribution

--- main.py
+++ main.py
@@ -15,16 +15,27 @@ class Course(object):
         # return "%s, %s, %s, %s" %(self.name, self.hours, self.diff_rate, self.time_consump)
         return str(self.name)
 
+class Semester(object):
+
+    def __init__(self, number):
+        self.number = number
+        self.class_lst = []
+        self.total_rating = 0
+
+    def __str__(self):
+        # return "%s, %s, %s, %s" %(self.name, self.hours, self.diff_rate, self.time_consump)
+        return str(self.number + 1)
+
+
 def main():
     # Open file
     in_file = open("./test.txt" , "r")
 
     # Get rid of headers
     for i in range(4):
-    	in_file.readline()
+        in_file.readline()
 
     lst = [] # List of courses
-    plan = {} # Dictionary of semester:course to take
 
     # Parses through file and creates instances of Course class
     for line in in_file:
@@ -36,35 +47,48 @@ def main():
     # Close file
     in_file.close()
 
-    # Sorts by rating from largest to smallest
-    for i in range(len(lst)):
-        for j in range(i):
-            if lst[i].rating > lst[j].rating:
-                lst[i], lst[j] = lst[j], lst[i]
+    # Sorts to prioritize largest ratings so that small ones can be used for evening out
+    lst = sorted(lst, key=lambda Course: Course.rating, reverse=True)
 
     # Prints course and its rating
     for item in lst:
         print(item, item.rating)
 
+    # Inputing number of semesters and initializing plan of Semesters
     num_semesters = int(input("enter # of semesters: "))
-    plan = {}
+    plan = []
 
-    for i in range(num_semesters):
-        plan[i + 1] = []
-
-    i, j = 0, 1
-    while i < len(lst):
-        plan[j].append(lst[i])
-        j += 1
+    i = 0
+    while i < num_semesters:
+        plan.append(Semester(i))
         i += 1
-        if j == num_semesters + 1:
-            j = 1
 
-    for key in plan:
-        print(key,':', end = ' ')
-        for x in plan[key]:
+    # Adding classes to semesters to minimize differences in difficulty
+    i = 0
+    while i < len(lst):
+        plan = sorted(plan, key=lambda Semester: Semester.total_rating)
+        plan[0].class_lst.append(lst[i])
+        plan[0].total_rating += lst[i].rating
+        i += 1
+    plan = sorted(plan, key=lambda Semester: Semester.number)
+
+    # Printing Semesters and corresponding classes
+    i = 0
+    while i < len(plan):
+        print(plan[i],':', plan[i].total_rating, end = ' ')
+        for x in plan[i].class_lst:
             print(x, end = " ")
         print()
+        i += 1
+
+    '''
+    1) add algorithm for handling prerequisits
+    2) algo for handling hardcoded class placements
+    3) algo for handling semester availabilities
+    4) conflicting classes
+    5) change input to excel sheet
+    '''
+
 
 
 main()
