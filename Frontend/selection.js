5cef600ae36c08b46a3034cda64d4d74ff5802cf
Aditya-Kharosekar <adityakharosekar@gmail.com>
2018-04-26 23:16:20 -0500

    added primary core course choices, and primary core electives

---
+++ Frontend/selection.js
@@ -0,0 +1,108 @@
+var CommNetsCourses = [
+	"EE 325: Electromagnetic Engineering",
+	"EE 325K: Antennas and Wireless Propagation",
+	"EE 445S: Real-Time Digital Signal Processing Laboratory",
+	"EE 351M: Digital Signal Processing",
+	"EE 360C: Algorithms",
+	"EE 360K: Introduction to Digital Communications",
+	"EE 461P: Introduction to Data Mining/Data Science Principles",
+	"EE 362K: Introduction to Automatic Control",
+	"EE 363M: Microwave and Radio Frequency Engineering",
+	"EE 471C: Wireless Communications Laboratory",
+	"EE 371R: Digital Image and Video Processing",
+	"EE 460J: Data Science Laboratory"
+];
+
+var ICCourses = [
+	"EE 438K: Analog Electronics",
+	"EE 338L: Analog Integrated Circuit Design",
+	"EE 440: Microelectronics Fabrication Techniques Laboratory",
+	"EE 445L: Embedded Systems Design Laboratory",
+	"EE 445S: Real-Time Digital Signal Processing Laboratory",
+	"EE 460M: Digital Systems Design Using HDL",
+	"EE 460N: Computer Architecture",
+	"EE 460R: Introduction to VLSI Design",
+	"EE 361R: Radio Frequency Circuit Design",
+	"EE 363M: Microwave & Radio Frequency Engineering",
+	"EE 374K: Biomedical Electronic Instrument Design",
+	"EE 374L: Applications of Biomedical Engineering"
+];
+
+var EnergyCourses = [
+	"EE 339: Solid-State Electronic Devices",
+	"EE 339S: Solar Energy Conversion Devices",
+	"EE 341: Electric Drives and Machines",
+	"EE 362Q: Power Quality and Harmonics",
+	"EE 362R: Renewal Energy and Power Systems",
+	"EE 362S: Development of a Solar-powered Vehicle",
+	"EE 368L: Power Systems Apparatus and Laboratory",
+	"EE 369: Power Systems Engineering",
+	"EE 362G: Smart Grids",
+	"ME 337C: Intro to Nuclear Power Systems"
+];
+
+var fieldsCourses = [
+	"EE 325K: Antennas and Wireless Propagation",
+	"EE 334K: Quantum Theory of Engineering Materials",
+	"EE 341: Electric Drives and Machines",
+	"EE 347: Modern Optics",
+	"EE 348: Laser and Optical Engineering",
+	"EE 361R: Radio Frequency Circuit Design",
+	"EE 363M: Microwave and Radio Frequency Engineering",
+	"EE 363N: Engineering Acoustics",
+	"EE 369: Power Systems Engineering",
+	"EE 374K: Biomedical Electronic Instrument Design",
+	"EE 374L: Applications of Biomedical Engineering"
+]
+
+var NanoCourses = [
+	"EE 334K: Quantum Theory of Engineering Materials",
+	"EE 438 :Fundamentals of Electronic Circuits I Laboratory",
+	"EE 338L: Analog Integrated Circuit Design",
+	"EE 339S: Solar Energy Conversion Devices",
+	"EE 347: Modern Optics",
+	"EE 348: Laser and Optical Engineering",
+	"EE 460R: Introduction to VLSI Design",
+	"EE 340P: High-Throughput Nanopatterning"
+];
+
+var CompArchCourses = [
+	"EE 422C: Software Design & Implementation II",
+	"EE 445M: Embedded and Real-Time Systems Laboratory",
+	"EE 445S: Real-Time Digital Signal Processing Laboratory",
+	"EE 460M: Digital Systems Design Using HDL",
+	"EE 360P: Concurrent and Distributed Systems",
+	"EE 460R: Introduction to VLSI Design",
+	"EE 361C: Multicore Computing",
+	"EE 461S: Operating Systems",
+	"EE 362K: Introduction to Automatic Control",
+	"EE 379K: Information Security & Privacy"
+];
+
+var SoftwareCourses = [
+	"EE 316: Digital Logic Design",
+	"EE 445L: Embedded Systems Design Laboratory",
+	"EE 445M: Embedded and Real-Time Systems Laboratory",
+	"EE 360F: Intro to Software Engineering",
+	"EE 460N: Computer Architecture",
+	"EE 360P: Concurrent and Distributed Systems",
+	"EE 360T: Software Testing",
+	"EE 361C: Multicore Computing",
+	"EE 461P: Data Science Principles",
+	"EE 361Q: Requirements Engineering",
+	"EE 461S: Operating Systems",
+	"EE 460J: Data Science Laboratory",
+	"EE 379K: Information Security & Privacy"
+];
+
+var DataScienceCourses = [
+	"EE 422C: Software Design & Implementation II",
+	"EE 445S: Real-Time Digital Signal Processing Laboratory",
+	"EE 360P: Concurrent and Distributed Systems",
+	"EE 361C: Multicore Computing",
+	"EE 461L: Software Engineering and Design Laboratory",
+	"EE 362K: Introduction to Automatic Control",
+	"EE 471C: Wireless Communications Laboratory",
+	"EE 371R: Digital Image and Video Processing",
+	"EE 379K: Architecture for Big Data Science"
+]
