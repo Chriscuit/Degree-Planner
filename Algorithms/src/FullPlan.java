503929ba40dce5b9613ff807a0ee111ae17c2bc8
Christopher R. Gill <chrisraygill@utexas.edu>
2018-04-28 16:41:55 -0500

    updated TODO statements

--- Algorithms/src/FullPlan.java
+++ Algorithms/src/FullPlan.java
@@ -2,4 +2,5 @@ public class FullPlan {
 
     // TODO: populate with some Semesters n' shit
     // TODO: algo for placing courses based on prereqs and difficulty
+    // TODO: Probably implement or create new class to convert what I make into JSON format to give to the middle guy
 }
