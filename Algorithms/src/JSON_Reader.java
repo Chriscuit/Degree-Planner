4f5398dd9e82367157b3df89ede8bf9620cc134d
Christopher R. Gill <chrisraygill@utexas.edu>
2018-04-28 16:12:22 -0500

    Modified courseinfo.json to make course object format. It is now an array of course objects now. Parser has a bug tho. I would like to convert the JSON to a list of JSONcourse objects.

--- Algorithms/src/JSON_Reader.java
+++ Algorithms/src/JSON_Reader.java
@@ -1,11 +1,35 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.List;
+
 public class JSON_Reader {
 
     // TODO: figure out how tf we gonna read this fucker
 
-    private String path;
+//    private String path;
 
-    JSON_Reader(String path) {
-        this.path = path;
+    public static void main(String[] args) throws Exception {
+
+        Object obj = new JSONParser().parse(new FileReader("/Users/christophergill/Downloads/Degree-Planner/courseInfo.json"));
+
+        JSONObject jsonObject = (JSONObject) obj;
+
+        JSONArray courses = (JSONArray) jsonObject.get("courses");
+        Iterator i = courses.iterator();
+
+        while (i.hasNext()) {
+            System.out.println(i.next() + "\n\n");
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        TypeReference<List<JSONcourse>> mapType = new TypeReference<List<JSONcourse>() {};
+        List<JSONcourse> jsonToPersonList = objectMapper.readValue(courses, mapType);
     }
-
 }
---
+++ Algorithms/src/JSONcourse.java
@@ -0,0 +1,64 @@
+import com.fasterxml.jackson.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "title",
+        "prerequisites",
+        "corequisites"
+})
+public class JSONcourse {
+
+    @JsonProperty("title")
+    private String title;
+    @JsonProperty("prerequisites")
+    private List<String> prerequisites = null;
+    @JsonProperty("corequisites")
+    private List<String> corequisites = null;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("title")
+    public String getTitle() {
+        return title;
+    }
+
+    @JsonProperty("title")
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @JsonProperty("prerequisites")
+    public List<String> getPrerequisites() {
+        return prerequisites;
+    }
+
+    @JsonProperty("prerequisites")
+    public void setPrerequisites(List<String> prerequisites) {
+        this.prerequisites = prerequisites;
+    }
+
+    @JsonProperty("corequisites")
+    public List<String> getCorequisites() {
+        return corequisites;
+    }
+
+    @JsonProperty("corequisites")
+    public void setCorequisites(List<String> corequisites) {
+        this.corequisites = corequisites;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
